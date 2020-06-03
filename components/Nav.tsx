1065751707cc48e8cee122823dcfa34716787151
errc sool <errcsool@engineer.com>
2020-06-03 02:28:05 -0400

    uploading for Vercel linking

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,22 @@
 import { AppBar, Toolbar } from '@material-ui/core'
-import styled from 'styled-components'
-
-const A = styled.a`
-    text-decoration: none;
-    color: #FFFFFF;
-`
 
 const Nav = () => {
     return (
         <AppBar position="fixed">
             <Toolbar variant="dense">
-                <A href="https://errcsool.com/portfolio/ab-test-service">
+                <a href="https://errcsool.com/portfolio/ab-test-service">
                     AAAB
-                </A>
+                </a>
+                <style jsx>{`
+                    a {
+                        text-decoration: none;
+                        color: #FFFFFF;
+                    }
+
+                    a:hover {
+                        text-decoration: underline;
+                    }
+                `}</style>
             </Toolbar>
         </AppBar>
     )
